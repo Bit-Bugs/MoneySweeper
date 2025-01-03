3a00440122abed8b2fdf43b1b64aa4f22aad0050
paul <paul0802@icloud.com>
2025-01-03 15:38:47 +0100

    added scoreboard to controller

--- src/main/java/bitbugs/moneysweeper/gui/MenuController.java
+++ src/main/java/bitbugs/moneysweeper/gui/MenuController.java
@@ -1,11 +1,10 @@
 package bitbugs.moneysweeper.gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 
 public class MenuController {
     @FXML
@@ -13,6 +12,15 @@ public class MenuController {
 
     @FXML
     private ToggleButton customDifficulty;
+
+    @FXML
+    private ToggleButton difficultyEasy;
+
+    @FXML
+    private ToggleButton difficultyMid;
+
+    @FXML
+    private ToggleButton difficultyHard;
 
     @FXML
     private TextField bombs;
@@ -27,18 +35,33 @@ public class MenuController {
     private Button playButton;
 
     @FXML
+    private ListView<String> scoreboard;
+
+    private final ObservableList<String> scoreboardItems = FXCollections.observableArrayList();
+
+    @FXML
     public void initialize() {
+        scoreboard.setItems(scoreboardItems);
+        //set scoreboard for default selected gamemode (easy)
+        scoreboardItems.add("ByteMe 🤖");
+        scoreboardItems.add("MemoryLeakMaster5000");
+
         // Automatically select the ToggleButton when bombs (custom difficulty) are entered
         bombs.focusedProperty().addListener((observable, oldValue, newValue) -> {
             customDifficulty.setSelected(true);
         });
 
         difficulty.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
-            if(difficulty.getSelectedToggle() != customDifficulty) {
+            if (difficulty.getSelectedToggle() != customDifficulty) {
                 bombs.setText("");
             }
 
             //Update Scoreboard here
+            scoreboardItems.clear();
+            if (difficulty.getSelectedToggle() == difficultyEasy) {
+                scoreboardItems.add("ByteMe 🤖");
+                scoreboardItems.add("MemoryLeakMaster5000");
+            }
         });
     }
 
