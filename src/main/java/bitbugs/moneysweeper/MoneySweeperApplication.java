11fac4340c6a42a1ea9711dc5dfaeee0eebd526e
paul <paul0802@icloud.com>
2025-01-05 18:50:52 +0100

    added app icon

--- src/main/java/bitbugs/moneysweeper/MoneySweeperApplication.java
+++ src/main/java/bitbugs/moneysweeper/MoneySweeperApplication.java
@@ -2,7 +2,11 @@ package bitbugs.moneysweeper;
 
 import bitbugs.moneysweeper.gui.SceneManager;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import javax.swing.ImageIcon;
+import java.awt.Taskbar;
 
 public class MoneySweeperApplication extends Application {
     @Override
@@ -10,6 +14,16 @@ public class MoneySweeperApplication extends Application {
         var sceneManager = SceneManager.getInstance();
         sceneManager.setPrimaryStage(stage, "Money Swe3§€$eper");
         sceneManager.setScene("menu.fxml");
+        var iconPath = getClass().getResource("/bitbugs/moneysweeper/icon.png");
+
+        if (iconPath != null) {
+            stage.getIcons().add(new Image(iconPath.toString()));
+
+            //dock icon for macOS
+            final Taskbar taskbar = Taskbar.getTaskbar();
+            var image = new ImageIcon(iconPath).getImage();
+            taskbar.setIconImage(image);
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module bitbugs.moneysweeper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens bitbugs.moneysweeper.gui to javafx.fxml;
     exports bitbugs.moneysweeper;
