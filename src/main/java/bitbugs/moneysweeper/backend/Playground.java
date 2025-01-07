6f50cf20552b8a9851b32512a67223b391832a6a
Philipp <philipp.lintner2001@gmail.com>
2025-01-07 01:00:56 +0100

    implemented uncoverFields without recursion

--- src/main/java/bitbugs/moneysweeper/backend/Playground.java
+++ src/main/java/bitbugs/moneysweeper/backend/Playground.java
@@ -1,6 +1,6 @@
 package bitbugs.moneysweeper.backend;
 
-import java.util.Random;
+import java.util.*;
 
 public class Playground {
     private Field[][] fields;
@@ -122,18 +122,105 @@ public class Playground {
         }
     }
 
-    // Covers/Uncovers one Tile
-    public int uncoverTile(int xPos, int yPos){
-        fields[xPos][yPos].setTurnedOver(true);
-        return fields[xPos][yPos].getSurroundingMines();
+private class Position{
+       public int x;
+       public int y;
+       public boolean isChecked;
+       public int surroundingMines;
+       public Position(int x, int y, boolean isChecked, int surroundingMines) {
+           this.x = x;
+           this.y = y;
+           this.isChecked = isChecked;
+           this.surroundingMines = surroundingMines;
+       }
+}
+
+
+    public ArrayList<Field> calculateUncoverFields(int xPos, int yPos) {
+        ArrayList<Field> result = new ArrayList<>();
+
+        if (xPos < 0 || yPos >= difficultySize) {
+            return result; //-> exit empty array out of range
+        }
+        if (fields[xPos][yPos].getTurnedOver())
+        {
+            //noch alle aufklappen die eh klar sind
+            return result;  //-> exit empty array (field is still turned)
+        }
+
+        if (fields[xPos][yPos].getHasBomb()) {
+            result.add(fields[xPos][yPos]); //-> exit with bomb as list item
+            return result;
+        }
+
+        if (fields[xPos][yPos].getSurroundingMines() > 0){
+            result.add(fields[xPos][yPos]); //-> exit with this field
+            return result;
+        }
+
+        //nur mehr der Fall dass man ein Field geklickt hat dass keine nummer hat und keine bombe ist
+        ArrayList<Position> calcList = new ArrayList<Position>();
+
+        //berechnet alle Felder die aufgedeckt werden
+        addSurroundingFields(xPos, yPos, calcList);
+
+        //result list aufbauen
+        for (Position position : calcList) {
+            result.add(fields[position.x][position.y]);
+        }
+
+        return result;
+
     }
 
-    //Uncovers all Tiles in the Field Array
-    public void UncoverAllTiles(){
-        for (int row = 0; row < fields.length; row++) {
-            for (int column = 0; column < fields[row].length; column++) {
-                uncoverTile(column, row);
+
+    private void addSurroundingFields(int xPos, int yPos,  ArrayList<Position> calcList){
+
+        addOnlyNewFieldsToList(xPos, yPos, true,calcList);  //mitte mitte
+
+        addOnlyNewFieldsToList(xPos-1,yPos-1, false, calcList); //links oben
+        addOnlyNewFieldsToList(xPos-1, yPos, false, calcList); //links mitte
+        addOnlyNewFieldsToList(xPos-1, yPos+1, false, calcList); //links unten
+        addOnlyNewFieldsToList(xPos, yPos-1, false, calcList); //mitte oben
+        addOnlyNewFieldsToList(xPos, yPos+1, false, calcList); //mitte unten
+        addOnlyNewFieldsToList(xPos+1, yPos-1,false, calcList); //rechts oben
+        addOnlyNewFieldsToList(xPos+1,yPos,false, calcList); //rechts mitte
+        addOnlyNewFieldsToList(xPos+1,yPos+1,false, calcList); //rechts unten
+    }
+
+    private void addOnlyNewFieldsToList(int x, int y, boolean checked, ArrayList<Position> calcList) {
+        if ( !isInRange(x,y) ) {
+            return;
+        }
+        for (Position position : calcList) {
+            if (position.x == x && position.y == y){
+                return; //pos existiert schon in list
             }
         }
+        calcList.add(new Position(x, y, checked, fields[x][y].getSurroundingMines()));
     }
+
+    private boolean isInRange(int x, int y) {
+        if (x<0 || y<0 ) {
+            return false;
+        }
+        if (x >= getDifficultySize() || y >= getDifficultySize() ) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+//    public void UncoverAllTiles(){
+//        for (int row = 0; row < fields.length; row++) {
+//            for (int column = 0; column < fields[row].length; column++) {
+//                uncoverTile(column, row);
+//            }
+//        }
+//    }
 }
